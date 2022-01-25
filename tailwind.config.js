1566c6f871c4b7afccb057a2506960874b96d738
Rashadul Alam <rashadul91@gmail.com>
2022-01-25 08:54:52 +0600

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily : {
+        poppins: "Poppins, sans-serif",
+        roboto: "Roboto, sans-serif",
+
+      },
+      screens: {
+        sm: '576px',
+        md: '768px',
+        lg: '992px',
+        xl: '1200px'
+      },
+      container: {
+        center: true,
+        padding: '1rem',
+      },
+      colors: {
+        'primary' : '#FD3D57',
+      }
+    },  
+  },  
+  plugins: [],
+}
